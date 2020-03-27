ee8dc9918477bf71a79802857110227b4bc22d64
edmundo papi <edmundopierre@yahoo.com>
2020-03-27 11:38:06 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,5 +7,13 @@ def reverse_each_word(str)
      empty_array <<  value.reverse
   end
   empty_array.join(" ")
+end
 
+def reverse_each_word(str)
+    string_array = str.split
+    empty_array = []
+    string_array.collect do |value|
+       empty_array.push(value.reverse)
+    end
+    empty_array.join(" ")
 end
