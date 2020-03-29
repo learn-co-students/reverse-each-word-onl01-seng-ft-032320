c2f7fb9978dccb624031d5392e0455385a45f66e
Anthony DiPietrantonio <antdp425@aol.com>
2020-03-29 16:59:46 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word (sentence)
-  sentence = sentence.split
-  sentence.collect do |word|
+  reversed = sentence.split
+  reversed.collect do |word|
     word.reverse
   end.join(" ")
 end
